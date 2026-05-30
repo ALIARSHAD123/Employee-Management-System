09186131e55ff5dbb364efd572226f394b1b5aa6
Ali Arshad <muhammedaliarshad788@example.com>
2026-05-30 23:20:26 +0500

    two way binding

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,15 +1,44 @@
+import { useState } from "react";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    console.log("emailiis", email);
+    console.log("password is", password);
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <div className="border-2 rounded-xl border-emerald-500 p-20 ">
-        <form className="flex flex-col items-center justify-center">
-          <input required
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+          className="flex flex-col items-center justify-center"
+        >
+          <input
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
             className="  border-2 border-emerald-500 text-xl py-3 px-3 rounded-full bg-transparent m-4 outline-none  placeholder:text-gray-400"
             type="email"
             placeholder="Enter Your Email"
           />
 
-          <input required
+          <input
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            required
             className=" border-2 border-emerald-500 text-xl py-3 px-3 rounded-full bg-transparent outline-none  placeholder:text-gray-400"
             type="password"
             placeholder="Enter Password"
